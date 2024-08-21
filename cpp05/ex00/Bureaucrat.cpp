de69242f352e9737ebaf785e9cd0f8633854c739
Gabriel Paez Garcia <gpaez-ga@c3r10s2.42malaga.com>
2024-08-21 19:06:08 +0200

    cpp05

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -2,4 +2,26 @@
 
 Bureaucrat::Bureaucrat() : _name("void") {}
 Bureaucrat::Bureaucrat(const Bureaucrat &src) { *this = src; }
-Bureaucrat &Bureaucrat::operator=(conts Bureaucrat &src)
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &src) {}
+Bureaucrat::~Bureaucrat() {}
+
+std::string Bureaucrat::getName() const { return this->_name; }
+//void Bureaucrat::setName(std::string name) { this->_name = name; }
+int Bureaucrat::getGrade() const { return this->_grade; }
+
+void Bureaucrat::GradeTooHighException(int grade)
+{
+    if (this->_grade < grade) throw
+        std::runtime_error("grade too high");
+}
+void Bureaucrat::GradeTooLowException(int grade)
+{
+    if (this->_grade > grade) throw
+        std::runtime_error("grade too low");
+}
+
+std::ofstream &operator<<(std::ofstream &o, const Bureaucrat &src)
+{
+    std::cout << src.getName() << ", bureaucrat grade " << src.getGrade();
+    return o;
+}
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -5,11 +5,21 @@
 class Bureaucrat
 {
     private:
-        const std::string _name;
-
+        const   std::string _name;
+        int     _grade;
     public:
         Bureaucrat();
         Bureaucrat(const Bureaucrat &src);
         Bureaucrat &operator=(const Bureaucrat &src);
         ~Bureaucrat();
+
+        std::string getName() const;
+        //void setName(std::string name);
+        int getGrade() const;
+        //void setGrade(int grade);
+
+        void GradeTooHighException(int grade);
+        void GradeTooLowException(int grade);
 };
+
+std::ofstream &operator<<(std::ofstream &o, const Bureaucrat &src);
