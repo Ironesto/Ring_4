0ddeddea8d234e57760114c6e05f324a86b24a1f
Ironesto <gabriel.p.g@hotmail.es>
2024-09-11 00:46:25 +0200

    cpp07done

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -2,8 +2,11 @@
 
 int main()
 {
+	std::cout << "string test" << std::endl;
 	std::string array[3] = {"array 1", "length 23", "finish all"};
 	iter(array, 3, func);
+	std::cout << std::endl;
+	std::cout << "int test" << std::endl;
 	int num[4] = {1, 2, 3, 42};
 	iter(num, 4, func);
 	return 0;
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -2,15 +2,40 @@
 
 int main()
 {
-	try
 	{
-		Array<std::string> str(3);
-		str[5] = "hola";
-		std::cout << str[0] << std::endl;
+		try
+		{
+			std::cout << "\t--Test 1--" << std::endl;
+			int n = 3;
+			Array<std::string> str(n);
+			str[0] = "hola";
+			str[1] = "mundo";
+			str[2] = "que tal";
+			for (int i = 0; i <= n; i++)
+				std::cout << "str[" << i << "]: " << str[i] << std::endl;
+		}
+		catch(const std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
-	catch(const std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		try
+		{
+			std::cout << "\t--Test 2--" << std::endl;
+			int n = 4;
+			Array<int> num(n);
+			num[0] = 0;
+			num[1] = 1;
+			num[2] = 123;
+			num[3] = 42;
+			for (int i = 0; i <= n; i++)
+				std::cout << "num[" << i << "]: " << num[i] << std::endl;
+		}
+		catch(const std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
 	return 0;
 }
