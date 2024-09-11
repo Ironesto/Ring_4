608085ea9b5ccf761eaa88eea940de6bf7b7614e
Gabriel Paez Garcia <gpaez-ga@c3r3s6.42malaga.com>
2024-09-11 17:56:58 +0200

    fixes

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,40 +2,43 @@
 
 int main()
 {
-	try
 	{
-		std::vector<int> num;
-		num.reserve(4);
-		num.push_back(11);
-		num.push_back(22);
-		num.push_back(33);
-		num.push_back(44);
-		//std::cout << "size " << num.capacity() << std::endl;
-		int res =  easyfind(num, 22);
-		std::cout << res << std::endl;
-		res =  easyfind(num, 2);
-		std::cout << res << std::endl;
+		try
+		{
+			std::list<int> num;
+			num.push_back(11);
+			num.push_back(22);
+			num.push_back(33);
+			num.push_back(44);
+			//std::cout << "size " << num.capacity() << std::endl;
+			int res =  easyfind(num, 22);
+			std::cout << res << std::endl;
+			res =  easyfind(num, 2);
+			std::cout << res << std::endl;
+		}
+		catch (std::string &e)
+		{
+			std::cout << e;
+		}
 	}
-	catch (std::string &e)
-	{
-		std::cout << e;
-	}
-	try
-	{
-		std::list<int> num;
-		num.push_back(11);
-		num.push_back(22);
-		num.push_back(33);
-		num.push_back(44);
-		//std::cout << "size " << num.capacity() << std::endl;
-		int res =  easyfind(num, 22);
-		std::cout << res << std::endl;
-		res =  easyfind(num, 2);
-		std::cout << res << std::endl;
-	}
-	catch (std::string &e)
-	{
-		std::cout << e;
+	{			//TERMINAR
+		try
+		{
+			std::list<int> num;
+			num.push_back(11);
+			num.push_back(22);
+			num.push_back(33);
+			num.push_back(44);
+			//std::cout << "size " << num.capacity() << std::endl;
+			int res =  easyfind(num, 22);
+			std::cout << res << std::endl;
+			res =  easyfind(num, 2);
+			std::cout << res << std::endl;
+		}
+		catch (std::string &e)
+		{
+			std::cout << e;
+		}
 	}
 	return 0;
 }
