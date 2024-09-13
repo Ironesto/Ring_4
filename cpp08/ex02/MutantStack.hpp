a7dafa98a6baaa638679c934bce2501895a2b650
Ironesto <gabriel.p.g@hotmail.es>
2024-09-13 04:44:46 +0200

    cpp08

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -9,10 +9,25 @@ class MutantStack : public std::stack<T>
 	private:
 
 	public:
-		MutantStack() {};
-		MutantStack(const MutantStack &src) { *this = src };
-		MutantStack &operator=(const MutantStack &src) { (void)src; return *this; };
-		~MutantStack() {};
+		MutantStack() {}
+		MutantStack(const MutantStack &src) { *this = src; }
+		MutantStack &operator=(const MutantStack &src) { (void)src; return *this; }
+		~MutantStack() {}
 
+		typedef typename std::stack<T>::container_type::iterator iterator;
 
+		iterator begin();
+		iterator end();
 };
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin(void)
+{
+	return (std::stack<T>::c.begin());
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end(void)
+{
+	return (std::stack<T>::c.end());
+}
