90b92f560b2604661dfca32a8ab8575e747b6b75
Gabriel Paez Garcia <gpaez-ga@c3r10s6.42malaga.com>
2024-09-20 19:29:48 +0200

    fixex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -9,6 +9,13 @@ void iter(T *array, size_t len, void(*f)(T &))
 		f(array[i]);
 }
 
+template <typename T>
+void iter(T *array, size_t len, void(*f)(const T &))
+{
+	for (size_t i = 0; i < len; i++)
+		f(array[i]);
+}
+
 template<typename T>
 void func(T &str)
 {
